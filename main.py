031ac4f054c7842ab6ce80c69c6e60f32f197677
julien guinot <jngt@hotmail.fr>
2024-03-04 22:26:50 +0000

    cleaning up

--- main.py
+++
@@ -1,8 +0,0 @@
-# an example file that takes an audio file from the path and returns the beats and downbeats of the audio file
-
-from tracker import BeatTracker
-
-if __name__ == "__main__":
-    beat_tracker = BeatTracker()
-    beat_times, downbeat_times = beat_tracker('path/to/audio/file')
-    
